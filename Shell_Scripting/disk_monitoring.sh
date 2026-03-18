3c2fe69023334248fb3a06cd55bd40fe55ddbe8a
Lim-Abhi <itslimabh@gmail.com>
2026-03-18 17:03:19 +0000

    Disk Code Modified

--- Shell_Scripting/disk_monitoring.sh
+++ Shell_Scripting/disk_monitoring.sh
@@ -5,7 +5,6 @@ figlet "Disk Monitoring"
 diskUsage=$(df -h | grep '/dev/sdd' | awk '{print $5}' | tr -d %) #Select required disk and tr to remove %
 
 
-
 if [[ $diskUsage -gt 80 ]];
 then
 	echo "Disk Space is Running Out..."
